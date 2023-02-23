ce99cbd0332ca6121a37c4f35393749a2ab740e0
wiperi <boris1151@163.com>
2023-02-24 00:40:27 +0800

    1

---
+++ 用函数计算指数幂.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void calculate_exponent(int BasicNumber,int exponent);
+
+void calculate_exponent(int BasicNumber,int exponent)
+{
+    int n,result;
+    result = BasicNumber;
+
+
+    for (n=1; n<exponent; n++)
+    result = result * BasicNumber;
+
+    printf("%d", result);
+}
+
+
+int main(void)
+{
+    calculate_exponent(2, 4);
+
+    return 0;
+}
