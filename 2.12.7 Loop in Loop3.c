a13dbe2e2c64fd5f6e235f9e8c36db8e6ff8fa75
wiperi <boris1151@163.com>
2023-02-26 15:45:16 +0800

    1544

---
+++ 2.12.7 Loop in Loop3.c
@@ -0,0 +1,20 @@
+// 用while函数实现
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 3;
+    while (n >= 1)
+    {
+        int t=n;
+        while (t >= 1)
+        {
+            printf("Smile!");
+
+            t = t - 1;
+        }
+        printf("\n");
+
+        n = n - 1;
+    }
+}
