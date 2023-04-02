d6012f0b338ce0f92295bfee49e900175e5a74c1
Magician <solidtreepassing@gmail.com>
2023-04-03 00:55:25 +0800

    4/3

---
+++ 123.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 12;
+
+    int b[a];
+
+    printf("%d", b[2]);
+}
+
+
+int array[] = {1,2,3,4,5,6,7};
+char array[] = "How is going, G";
+
+int array2[40] = {[39] = 233};
+
+float rain[5][10];
+
+// 指针中，“*”读取地址上的值，“&”读取地址
+
