38f1cb74db46ddd7b10d28ab2c35094f450d1a75
Magician <solidtreepassing@gmail.com>
2023-04-16 17:20:45 +0800

    rename2

--- 123.c
+++ 123.c
@@ -1,9 +1,31 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
-int main() {
-    // Write C code here
-    int a[] = {1,2,3};
-    scanf("%d", a+1);
-    printf("%d", a[1]);
+void ex_3(void) {
+    for (int i = 0; i <= 5; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%c ", 65 + 5 - j);
+        }
+        printf("\n");
+    }
 }
+void ex_4(void) {
+    int k;
+    for (int i = 0; i <= 5; i++) {
+        for (int j = 0, k = i; j <= i; j++, k++) {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+}
+void ex_5(void) {
+    int k = 0;
+    for (int i = 0; i <= 5; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", k);
+            k++;
+        }
+        printf("\n");
+    }
+}
+
+int main() { ex_5(); }
