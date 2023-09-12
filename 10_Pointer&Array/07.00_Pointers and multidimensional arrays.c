7f3548c9bb734e5836e8b140642f9cb317d6f670
Magician <solidtreepassing@gmail.com>
2023-09-12 19:55:00 +0800

    up

--- 10_Pointer&Array/07.00_Pointers and multidimensional arrays.c
+++ 10_Pointer&Array/07.00_Pointers and multidimensional arrays.c
@@ -4,43 +4,50 @@
 #include <stdio.h>
 
 int main() {
-    int aa[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
+    // 声明并初始化一个二维数组
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+    // 打印二级数组中每一个元素的地址
     printf("first it is address\n\n");
-
     for (int i = 0; i < 2; i++) {
         for (int k = 0; k < 3; k++) {
-            printf("%p\n", &aa[i][k]);
+            printf("%p\n", &arr[i][k]);
         }
     }
 
     printf("\n");
 
+    // 打印一级数组中每一个元素的地址
     for (int i = 0; i < 2; i++) {
-        printf("%p\n", &aa[i]);
+        printf("%p\n", &arr[i]);
     }
 
     printf("\n");
 
-    printf("%p\n", &aa);
+    // 打印数组的地址
+    printf("%p\n", &arr);
 
     printf("\nthen it is value\n\n");
 
+    // 打印二级数组中每一个元素的值
     for (int i = 0; i < 2; i++) {
         for (int k = 0; k < 3; k++) {
-            printf("%p\n", aa[i][k]);
+            printf("%p\n", arr[i][k]);
         }
     }
 
     printf("\n");
 
+    // 打印一级数组中每一个元素（子数组）的值
     for (int i = 0; i < 2; i++) {
-        printf("%p\n", aa[i]);
+        printf("%p\n", arr[i]);
     }
 
     printf("\n");
 
-    printf("%p\n", aa);
+    // 打印数组名称对应的值
+    printf("%p\n", arr);
 
     return 0;
 }
