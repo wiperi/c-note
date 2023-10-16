c9296b7e96601a40a35c01cc2da028f8ce617a4b
wiperi <solidtreepassing@gmail.com>
2023-10-17 02:48:40 +0800

    up

--- C01_SelfWork/15_Digit_Of_Number.c
+++ C01_SelfWork/15_Digit_Of_Number.c
@@ -1,5 +1,41 @@
 // 用户输入数字，判断该数字为几位数
 #include <stdio.h>
+#include <math.h>
+
+int countDigits(double num) {
+    if (num < 0) {
+        num = -num;  // 将负数转为正数以简化处理
+    }
+
+    int digits = 0;
+
+    // 如果 num 是 0，则它有一位
+    if (num < 1) {
+        digits = 1;
+    } else {
+        while (num >= 1) {
+            num /= 10;
+            digits++;
+        }
+    }
+
+    return digits;
+}
+
+int countDigits(double num) {
+    if (isnan(num) || isinf(num)) {
+        // 如果是 NaN 或无穷大，则返回 0
+        return 0;
+    }
+
+    // 将负数转为正数以简化处理
+    num = fabs(num);
+
+    // 使用 log10 函数来计算数字的位数
+    int digits = (int)log10(num) + 1;
+
+    return digits;
+}
 
 int main(void)
 {
