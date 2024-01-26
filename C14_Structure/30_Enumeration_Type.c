a5b0af4f9b5f152122f230cf45575cb3824549c7
Magician <solidtreepassing@gmail.com>
2024-01-26 22:33:05 +1100

    enum update

--- C14_Structure/30_Enumeration_Type.c
+++ C14_Structure/30_Enumeration_Type.c
@@ -9,6 +9,13 @@ enum spectrum { red, orange, yellow, green, blue, violet };
 const char* colors[] = {"red", "orange", "yellow", "green", "blue", "violet"};
 #define LEN 30
 
+/**
+ * @brief This function prompts the user to enter colors and displays corresponding messages.
+ *
+ * The function asks the user to enter a color and then checks if the entered color is valid.
+ * If the color is valid, it displays a corresponding message. If the color is not valid,
+ * it informs the user that it doesn't know about the entered color.
+ */
 int main(void) {
     char choice[LEN];
     enum spectrum color;
@@ -51,6 +58,17 @@ int main(void) {
     return 0;
 }
 
+/**
+ * @brief Reads a string from the standard input and removes the newline character.
+ *
+ * This function reads a string from the standard input using fgets() function.
+ * It removes the newline character from the string if present.
+ * If the newline character is not found, it clears the remaining characters from the input buffer.
+ *
+ * @param st The character array to store the input string.
+ * @param n The maximum number of characters to read.
+ * @return A pointer to the input string if successful, NULL otherwise.
+ */
 char* s_gets(char* st, int n) {
     char* ret_val;
     char* find;
